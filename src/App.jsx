0f155aa937b60364001add1dc5074ad6a7bc9b43
tomita_kazuki <tmtkzk@icloud.com>
2022-09-20 18:14:26 +0900

    Add: Footerの雛形を作成、アプリの背景を選択

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { Header } from './components/atoms/layouts/Header'
+import { Footer } from './components/atoms/layouts/Footer'
 
 export const App = () => {
   return (
     <>
       <Header/>
+      <Footer/>
     </>
   )
 }
