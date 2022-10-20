009a8827dd070f0399d3157611b066bbca641006
tomita_kazuki <tmtkzk@icloud.com>
2022-10-20 15:09:33 +0900

    Remove: 使用していないhooksを削除

--- src/hooks/useGoogleMap.js
+++ src/hooks/useGoogleMap.js
@@ -1,4 +1,4 @@
-import { useState, useCallback, useEffect } from 'react'
+import { useState, useCallback } from 'react'
 import { useJsApiLoader } from '@react-google-maps/api'
 
 export const useGoogleMap = () => {
