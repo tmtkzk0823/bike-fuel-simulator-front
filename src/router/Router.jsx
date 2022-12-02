a1018785d8dac98f10e5dfeeec3eb98d1a8f7c35
tomita_kazuki <tmtkzk@icloud.com>
2022-12-02 18:52:13 +0900

    Add: ログインフォームまでのルーティングを設定

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,11 +1,12 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import { Top } from '@/components/pages'
+import { Top, LoginForm } from '@/components/pages'
 
 export const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Top />} />
+        <Route path="/login" element={<LoginForm />} />
       </Routes>
     </BrowserRouter>
   )
