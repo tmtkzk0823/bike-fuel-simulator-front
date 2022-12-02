feba403fa19f4876e387e1e61adeb713604b81eb
tomita_kazuki <tmtkzk@icloud.com>
2022-12-02 18:33:59 +0900

    Add: サイドバーの雛形を作成

--- src/components/elements/layouts/Header.jsx
+++ src/components/elements/layouts/Header.jsx
@@ -1,45 +1,111 @@
 import { memo, useState } from 'react'
 // mui
-import Box from '@mui/material/Box'
-import Toolbar from '@mui/material/Toolbar'
-import Typography from '@mui/material/Typography'
-import IconButton from '@mui/material/IconButton'
+import {
+  Box,
+  Typography,
+  Toolbar,
+  IconButton,
+  Drawer,
+  List,
+  ListItem,
+  ListItemButton,
+} from '@mui/material'
 import MenuIcon from '@mui/icons-material/Menu'
+import AccountCircleIcon from '@mui/icons-material/AccountCircle'
 
 export const Header = memo(() => {
   const [sideBarOpenFlag, setSideBarOpenFlag] = useState(false)
+
   return (
-    <Box
-      sx={{
-        color: '#444444',
-        backgroundColor: '#d3d3d3',
-        p: 2,
-      }}
-    >
-      <Toolbar>
-        <IconButton
-          size="large"
-          edge="start"
-          color="inherit"
-          aria-label="menu"
-          sx={{ ml: 1 }}
-        >
-          <MenuIcon />
-        </IconButton>
-        <Typography
-          variant="h5"
-          component="div"
-          align="center"
+    <>
+      <Drawer
+        anchor="left"
+        open={sideBarOpenFlag}
+        onClose={() => setSideBarOpenFlag(false)}
+      >
+        <Box
           sx={{
-            flexGrow: 1,
-            fontWeight: 'bold',
-            fontStyle: 'italic',
-            fontSize: 30,
+            width: 250,
           }}
         >
-          バイク航続距離シミュレーター
-        </Typography>
-      </Toolbar>
-    </Box>
+          <List>
+            <ListItem
+              sx={{
+                color: 'primary.main',
+              }}
+            >
+              <AccountCircleIcon />
+              <ListItemButton>Login</ListItemButton>
+            </ListItem>
+            <ListItem
+              sx={{
+                color: 'primary.main',
+              }}
+            >
+              <ListItemButton>マイページ</ListItemButton>
+            </ListItem>
+            <ListItem
+              sx={{
+                color: 'primary.main',
+              }}
+            >
+              <ListItemButton>プライバシーポリシー</ListItemButton>
+            </ListItem>
+            <ListItem
+              sx={{
+                color: 'primary.main',
+              }}
+            >
+              <ListItemButton>利用規約</ListItemButton>
+            </ListItem>
+            <ListItem
+              sx={{
+                color: 'primary.main',
+              }}
+            >
+              <ListItemButton>
+                お問合せ
+                <br />
+                （開発者twitter）
+              </ListItemButton>
+            </ListItem>
+          </List>
+        </Box>
+      </Drawer>
+
+      <Box
+        sx={{
+          color: '#444444',
+          backgroundColor: '#d3d3d3',
+          p: 2,
+        }}
+      >
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ ml: 1 }}
+            onClick={() => setSideBarOpenFlag(true)}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography
+            variant="h5"
+            component="div"
+            align="center"
+            sx={{
+              flexGrow: 1,
+              fontWeight: 'bold',
+              fontStyle: 'italic',
+              fontSize: 30,
+            }}
+          >
+            バイク航続距離シミュレーター
+          </Typography>
+        </Toolbar>
+      </Box>
+    </>
   )
 })
