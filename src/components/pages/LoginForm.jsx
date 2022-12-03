e199d1c970b087bf8443f9cebe8e175c2e2b2c0e
tomita_kazuki <tmtkzk@icloud.com>
2022-12-04 01:02:04 +0900

    Fix: Boxないの要素を中央寄せに

--- src/components/pages/LoginForm.jsx
+++ src/components/pages/LoginForm.jsx
@@ -1,15 +1,28 @@
 import { Header } from '../elements/layouts/Header'
 import { Footer } from '../elements/layouts/Footer'
-import { Box } from '@mui/material'
+import { Box, Card } from '@mui/material'
 
 export const LoginForm = () => {
   return (
     <>
       <Header />
-      <Box>
-        <h1>LOGIN FORM</h1>
+      <Box
+        sx={{
+          minHeight: '100vh',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+      >
+        <Card
+          sx={{
+            width: '40%',
+            textAlign: 'center',
+          }}
+        >
+          <h1>LOGIN FORM</h1>
+        </Card>
       </Box>
-
       <Footer />
     </>
   )
