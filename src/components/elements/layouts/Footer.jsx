875010228b06c31d544242fdf016046bef5fc2eb
tomita_kazuki <tmtkzk@icloud.com>
2022-12-03 22:19:45 +0900

    Fix: Footerの横幅を修正

--- src/components/elements/layouts/Footer.jsx
+++ src/components/elements/layouts/Footer.jsx
@@ -7,16 +7,18 @@ export const Footer = memo(() => {
       sx={{
         backgroundColor: '#d3d3d3',
         color: '#ffffee',
-        p: 2,
-        b: 0,
         display: 'flex',
         justifyContent: 'space-evenly',
+        bottom: 0,
+        pt: 2,
+        pb: 2,
+        width: '100%',
       }}
     >
       <p>&copy;バイク航続距離シミュレーター</p>
       <p>プライバシーポリシー</p> {/*後にリンク(モーダル表示のイベント)を作成*/}
       <p>利用規約</p> {/*後にリンク(モーダル表示のイベント)を作成*/}
-      <p>twitter</p>{' '}
+      <p>twitter</p>
       {/*後に
     リンク(開発者twitter)を作成*/}
     </Box>
