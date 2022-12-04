240e6f0be3437b7a9b4f5c607190eafbcae780de
tomita_kazuki <tmtkzk@icloud.com>
2022-12-05 00:58:53 +0900

    Add: ログインフォーム作成

--- src/components/pages/LoginForm.jsx
+++ src/components/pages/LoginForm.jsx
@@ -1,42 +1,107 @@
+import { memo } from 'react'
+// component
 import { Header } from '../elements/layouts/Header'
 import { Footer } from '../elements/layouts/Footer'
-import { Box, Card, Button, Divider } from '@mui/material'
+// MUI
+import { Box, Card, Button, Divider, TextField, Link } from '@mui/material'
 import TwitterIcon from '@mui/icons-material/Twitter'
 
-export const LoginForm = () => {
+export const LoginForm = memo(() => {
   return (
     <>
       <Header />
       <Box
         sx={{
-          minHeight: '100vh',
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'center',
+          minHeight: '80vh',
         }}
       >
-        <Card
+        <Box
           sx={{
-            width: '40%',
-            textAlign: 'center',
+            mt: 15,
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
           }}
         >
-          <h2>ログイン</h2>
-
-          <Button
-            variant="contained"
+          <Card
             sx={{
-              mb: 2,
+              mt: 0,
+              width: '25%',
+              textAlign: 'center',
+              borderRadius: 10,
             }}
-            onClick={console.log('login処理')}
           >
-            <TwitterIcon />
-            Twitterでログイン
-          </Button>
-          <Divider variant="middle" />
-        </Card>
+            <h2>ログイン</h2>
+
+            <Button
+              variant="contained"
+              sx={{
+                mb: 2,
+                py: 1,
+              }}
+              onClick={() => console.log('login処理')}
+            >
+              <TwitterIcon />
+              Twitter ログイン
+            </Button>
+            <Divider variant="middle">または</Divider>
+
+            <Box
+              component="form"
+              sx={{
+                mt: 2,
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+              }}
+            >
+              <TextField
+                size="small"
+                margin="normal"
+                required
+                id="email"
+                label="メールアドレス"
+                name="email"
+                autoComplete="email"
+                focused
+              />
+
+              <TextField
+                size="small"
+                margin="normal"
+                required
+                name="password"
+                label="パスワード"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+                focused
+              />
+
+              <Button
+                type="submit"
+                variant="contained"
+                color="inherit"
+                sx={{ mt: 3, mb: 2 }}
+              >
+                ログイン
+              </Button>
+
+              <Link href="/">新規登録</Link>
+              <Link
+                href="/"
+                sx={{
+                  mt: 1,
+                  mb: 5,
+                }}
+              >
+                パスワードをお忘れの方はこちら
+              </Link>
+            </Box>
+          </Card>
+        </Box>
       </Box>
       <Footer />
     </>
   )
-}
+})
