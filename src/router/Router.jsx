38b41db46d6e06831baa63d6a7b5bbb8457ad88a
tomita_kazuki <tmtkzk@icloud.com>
2022-12-24 00:19:34 +0900

    Add: ログインしていないとmypageに行けないように設定

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,15 +1,55 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import { Top, LoginForm, SignUpForm, MyPage } from '@/components/pages'
 
-export const Router = () => {
+import { AuthContext } from '@/App'
+
+export const Router = (props) => {
+  const {
+    loading,
+    setLoading,
+    isSignedIn,
+    setIsSignIn,
+    currentUser,
+    setCurrentUser,
+  } = props
+
+  // 認証していなかった場合にルーティングを修正
+  const Private = (props) => {
+    const { children } = props
+
+    if (!loading) {
+      if (isSignedIn) {
+        return children
+      } else {
+        return <Navigate to="/login" />
+      }
+    } else {
+      return <></>
+    }
+  }
+
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Top />} />
-        <Route path="/login" element={<LoginForm />} />
-        <Route path="/sign_up" element={<SignUpForm />} />
-        <Route path="/mypage" element={<MyPage />} />
-      </Routes>
-    </BrowserRouter>
+    <AuthContext.Provider
+      value={{
+        loading,
+        setLoading,
+        isSignedIn,
+        setIsSignIn,
+        currentUser,
+        setCurrentUser,
+      }}
+    >
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Top />} />
+          <Route path="/login" element={<LoginForm />} />
+          <Route path="/sign_up" element={<SignUpForm />} />
+          <Route
+            path="/mypage"
+            element={<Private component={<MyPage />} redirect="/login" />}
+          />
+        </Routes>
+      </BrowserRouter>
+    </AuthContext.Provider>
   )
 }
