74609105ad6bbfa22f84f8f9560df7d7dc17e0aa
tomita_kazuki <tmtkzk@icloud.com>
2022-12-28 11:10:13 +0900

    Fix: mypageにheader,footerを配置

--- src/components/pages/MyPage.jsx
+++ src/components/pages/MyPage.jsx
@@ -1,3 +1,12 @@
+import { Header } from '@/components/elements/layouts/Header'
+import { Footer } from '@/components/elements/layouts/Footer'
+
 export const MyPage = () => {
-  return <h1>MyPage</h1>
+  return (
+    <>
+      <Header />
+      <h1>MyPage</h1>
+      <Footer />
+    </>
+  )
 }
