29454430eed565c08da7b2fee5f6720ef1caa5a1
tomita_kazuki <tmtkzk@icloud.com>
2023-01-06 23:50:34 +0900

    Fix: メモを追加

--- src/urls/index.js
+++ src/urls/index.js
@@ -4,6 +4,7 @@ export const manufacturersIndex = `${DEFAULT_API_URL}/manufacturers`
 export const bikesIndex = (manufacturersId) =>
   `${DEFAULT_API_URL}/manufacturers/${manufacturersId}/bikes`
 
+// 認証
 export const signUpUrl = `${DEFAULT_API_URL}/auth`
 export const userUpdateUrl = `${DEFAULT_API_URL}/auth`
 export const signInUrl = `${DEFAULT_API_URL}/auth/sign_in`
