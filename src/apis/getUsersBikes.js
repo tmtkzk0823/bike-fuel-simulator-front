531b25f37ebfabf364ff252f3cbda4e213ce138a
tomita_kazuki <tmtkzk@icloud.com>
2023-01-19 16:58:06 +0900

    Add: userの持っているバイクを取得する処理を作成

---
+++ src/apis/getUsersBikes.js
@@ -0,0 +1,21 @@
+// axios
+import axios from 'axios'
+// urls
+import { getUserBikesUrl } from '@/urls/index'
+//Cookies
+import Cookies from 'js-cookie'
+
+export const getUsersBikes = () => {
+  return axios
+    .get(getUserBikesUrl, {
+      headers: {
+        'access-token': Cookies.get('_access_token'),
+        client: Cookies.get('_client'),
+        uid: Cookies.get('_uid'),
+      },
+    })
+    .then((response) => {
+      return response
+    })
+    .catch((e) => console.log(e))
+}
